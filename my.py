eada8ff5a80f33c67b584c1575dea7491329387b
CHMS2021 <adekeyejoshua.t@gmail.com>
2021-07-11 15:34:53 -0500

    v1

---
+++ my.py
@@ -0,0 +1,12 @@
+'''
+
+write variables
+# Greetings= input(" What is your name?")
+# location = input("Where are you from")
+# print( "Welcome " + Greetings + " " + "you are from " + location)
+
+#Numbers
+Age =input("What is your age ?")
+print("your new age is " ,int(Age) +50)
+
+'''
